7223c5e742f7a5466fd97091621ca770b6a0f762
YaboSun <yabosun@163.com>
2020-04-09 17:07:38 +0800

    高级特性-列表生成式

---
+++ 高级特性/列表生成式.py
@@ -0,0 +1,17 @@
+"""
+生成[1*1,2*2,...5*5]的列表
+"""
+
+
+l = []
+for i in range(1, 6):
+    l.append(i * i)
+print(l)
+
+"""
+利用列表生成式实现
+"""
+print([x * x for x in range(1, 6)])
+
+# 生成全排列
+print([m + n for m in 'ABC' for n in 'EFG'])

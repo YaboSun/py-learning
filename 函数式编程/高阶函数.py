ddfbcdab0aadc1a7b4ff0688dbbae3d258f64ec2
YaboSun <yabosun@163.com>
2020-05-13 10:10:41 +0800

    高阶函数 map/reduce

--- 函数式编程/高阶函数.py
+++ 函数式编程/高阶函数.py
@@ -100,6 +100,6 @@ def str2float(s):
 
 print('str2float(\'123.456\') =', str2float('123.456'))
 if abs(str2float('123.456') - 123.456) < 0.00001:
-    print('测试成功!')
+    print('test passed!')
 else:
     print('测试失败!')

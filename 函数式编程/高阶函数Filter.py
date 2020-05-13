c764b38c6c637844ccf835fb51d55d1b7f8544da
YaboSun <yabosun@163.com>
2020-05-13 10:52:10 +0800

    高阶函数 filter

---
+++ 函数式编程/高阶函数Filter.py
@@ -0,0 +1,22 @@
+# 筛选回文数字
+def is_palindrome(n):
+    lisn = [int(x) for x in str(n)]
+    i = 0
+    j = len(lisn) - 1
+    while i <= j:
+        if lisn[i] == lisn[j]:
+            i = i + 1
+            j = j - 1
+        else:
+            return False
+    return True
+
+
+# 测试:
+output = filter(is_palindrome, range(1, 1000))
+print('1~1000:', list(output))
+if list(filter(is_palindrome, range(1, 200))) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 22, 33, 44, 55, 66, 77, 88, 99, 101,
+                                                  111, 121, 131, 141, 151, 161, 171, 181, 191]:
+    print('测试成功!')
+else:
+    print('测试失败!')
